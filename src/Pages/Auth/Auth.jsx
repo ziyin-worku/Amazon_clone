ea0d5fa55add5a2bedb196e95dc13b702f4597e3
ziyin-worku <ziyinworku@gmail.com>
2025-02-25 09:33:54 +0300

    Authentication functionality

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import classes from "./SignUp.module.css";
 import { Link } from "react-router";
+import { auth } from "../../Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { DataContext } from "../../Component/DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [{ user }, dispatch] = useContext(DataContext);
+
+  console.log(user)
+
+  const authHandler = async (e) => {
+    e.preventDefault();
+    // console.log(e.target.name);
+    if (e.target.name == "signin") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          // console.log(userInfo);
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          // console.log(userInfo);
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  // console.log(password, email);
+
   return (
     <section className={classes.login}>
       {/* logo  */}
@@ -19,25 +66,48 @@ function Auth() {
           <div>
             <label htmlFor="email">E-mail</label>
 
-            <input type="email" id="email" />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
 
-            <input type="password" id="password" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+            />
           </div>
 
-          <button className={classes.login_signInButton}>Sign in</button>
+          <button
+            type="submit"
+            name="signin"
+            onClick={authHandler}
+            className={classes.login_signInButton}
+          >
+            Sign in
+          </button>
           {/* agreement */}
-          
-          
         </form>
         <p>
-            By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our
-            Interest-Based Ads Notice.
-          </p>
-          {/* create account button */}
-          <button className={classes.login_registerButton}>Create your Amazon Account</button>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        {/* create account button */}
+        <button
+          type="submit"
+          name="signup"
+          onClick={authHandler}
+          className={classes.login_registerButton}
+        >
+          Create your Amazon Account
+        </button>
       </div>
     </section>
   );
--- src/Utility/reducer.jsx
+++ src/Utility/reducer.jsx
@@ -3,6 +3,7 @@ import { Type } from "./action.type";
 
 export const initialState = {
   basket: [],
+  user: null
 };
 
 export const reducer = (state, action) => {
@@ -48,6 +49,12 @@ export const reducer = (state, action) => {
         ...state,
         basket:newBasket
       }
+
+      case Type.SET_USER:
+        return {
+          ...state, 
+          user: action.user
+        }
 
     default:
       return state;
